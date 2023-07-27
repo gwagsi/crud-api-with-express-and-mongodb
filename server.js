655b2be079497e9d1d2ecae82a5f8843f6fc32b8
gwagsi <gwagsiglenn@gmail.com>
2023-07-27 14:22:24 +0100

    connect to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 3000
 
@@ -6,6 +7,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+
+mongoose.connect("mongodb+srv://root:243243243@cluster0.ukgpppk.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+    console.log("Connected to database successfully")    
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+        console.log(`App listening on port ${port}`)
+      })
+    
 })
+.catch((console.error()));
